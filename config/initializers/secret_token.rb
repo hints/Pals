228b5c5538413b80c8fcf95d4b8bdcc302afa236
Ricky Wong <rickwong@gmail.com>
2011-06-16 13:41:50 -0700

    ROR basics

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pals::Application.config.secret_token = 'aac8badc949e898e6aa37dcd0baa9063d6b0b7c742c1db25d72fe7c24fed4621ef2bbbffbf5254bad12b1b2e7f52abbd280945bb0b4dc9d35cd7d0e32f7c13ad'
